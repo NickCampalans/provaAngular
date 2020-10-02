7a2a2dff6ca844f06b4281e9327967c434634bc8
NickCampalans <nsunyer@institutcampalans.net>
2020-10-02 17:54:46 +0200

    Canvis del divendres 2/10/2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'provaAngular';
+export class AppComponent implements OnInit {
+  //numeros: number[] = [];
+  numeros = new Array<number>(1000);
+  comptadors = new Array<number>(21);
+
+  a: number;
+  
+  constructor() {
+    this.a = -1;
+    console.log("A = " + this.a);
+
+    // Inicialitzem les dades
+    this.initData();
+
+    // Calcular histograma
+    this.computeHistograms();
+  }
+
+  initData() {
+    // Inicialitzem els numeros.
+    /*for (let index = 0; index < 1000; index++) {
+      this.numeros.push(Math.round(Math.random() * 20));
+    }*/
+    for (let index = 0; index < this.numeros.length; index++) {
+      this.numeros[index] = Math.round(Math.random() * 20);
+    }
+
+    // Inicialitzem els comptadors
+    for (let index = 0; index < this.comptadors.length; index++) {
+      this.comptadors[index] =  0;
+    }
+  }
+
+  computeHistograms() {
+    for (let index = 0; index < this.numeros.length; index++) {
+      this.comptadors[this.numeros[index]] += 1;
+    }
+  }
+
+  ngOnInit() {
+
+  }
+
+  incA() {
+    this.a += 1;
+    console.log("A = " + this.a);
+    
+  }
 }
